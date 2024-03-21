771bacf8cbe9bdfcc02d8192a28435fedce8ef22
Qabool Mohammed <136561495+Nadejag@users.noreply.github.com>
2024-03-21 23:30:45 +0500

    Day1 coding challange #100dayscoding challange

--- main.js
+++
@@ -1,5 +0,0 @@
-// console.log("hello world");
-// var myname='ali';
-// var message='is a good person';
-
-// console.log(myname+""+message);
--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
-// console.log("hello world");
-// let myname='ali';
-// let message='is a good person';
-
-// console.log(myname+""+message);
+// //console.log(2===2);
+// //console.log("qa");
+// let A="apple";
+// const B="ball";
+// var C="cat";
+// A="apple1";
+// var C="cat2";
+// //const B="ball2";
+// console.log(A);
+// console.log(B);
+// console.log(C);
+let x=10;
+if(x===10){
+    let A="apple";
+const B="ball";
+ var C="cat";
+//
+console.log(A);
+}
+console.log(A);
